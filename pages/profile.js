a95851f178d6dbedb112b6fd8f418d8bc5c63327
jihunhong <redgee49@gmail.com>
2020-06-29 23:07:00 +0900

    회원가입 폼

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,25 @@
-import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditFrom';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
-    return (
+
+    const followerList = [{nickname : '지훈'}, {nickname : '채원'}];
+    const followingList = [{nickname : '지훈'}, {nickname : '채원'}];
+
+    return(
+        <>
+        <Head>
+            <title>내 프로필 | NodeBird</title>
+        </Head>
         <AppLayout>
-            <Head>
-                <title>내 프로필 | NodeBird</title>
-            </Head>
-            <div>내 프로필</div>
+            <NicknameEditForm />
+            <FollowList header="팔로잉 목록" data={followingList}/>
+            <FollowList header="팔로워 목록" data={followerList}/>
         </AppLayout>
-    
+        </>
     )
 }
 
