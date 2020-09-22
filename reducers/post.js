02f9bc4f59c50e53b990af19d6043dce03fb7be9
jihunhong <redgee49@gmail.com>
2020-09-22 22:28:05 +0900

    immer 리팩토링

--- reducers/post.js
+++ reducers/post.js
@@ -1,4 +1,5 @@
 import shortId from 'shortid';
+import produce from 'immer';
 
 export const initialState = {
   mainPosts: [{
@@ -90,81 +91,58 @@ const dummyComment = (data) => ({
 })
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST:
-      return {
-        ...state,
-        addPostLoading: true,
-        addPostDone: false,
-        addPostError: null,
-      };
-    case ADD_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: [dummyPost(action.data), ...state.mainPosts],
-        addPostLoading: false,
-        addPostDone: true,
-      };
-    case ADD_POST_FAILURE:
-      return {
-        ...state,
-        addPostLoading: false,
-        addPostDone: action.error,
-      };
 
-    case ADD_COMMENT_REQUEST:
-      return {
-        ...state,
-        addCommentLoading: true,
-        addCommentDone: false,
-        addCommentError: null,
-      };
-    case ADD_COMMENT_SUCCESS: {
-      const postIndex = state.mainPosts.findIndex((v) => v.id === action.data.postId);
-      const post = { ...state.mainPosts[postIndex] };
-      console.log(post);
-      post.Comments = [dummyComment(action.data.content), ...post.Comments];
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = post;
-      return {
-        ...state,
-        mainPosts,
-        addCommentLoading: false,
-        addCommentDone: true,
-      };
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case ADD_POST_REQUEST:
+        draft.addPostLoading = true;
+        draft.addPostDone = false;
+        draft.addPostError = null;
+        break;
+      case ADD_POST_SUCCESS:
+        draft.addPostLoading = false;
+        draft.addPostDone = true;
+        draft.mainPosts.unshift(dummyPost(action.data));
+        break;
+      case ADD_POST_FAILURE:
+        draft.addPostLoading = false;
+        draft.addPostDone = action.error;
+        break;
+      case REMOVE_POST_REQUEST:
+        draft.removePostLoading = true;
+        draft.removePostDone = false;
+        draft.removePostError = null;
+        break;
+      case REMOVE_POST_SUCCESS: {
+        draft.removePostLoading = false;
+        draft.removePostDone = true;
+        console.log(action.data);
+        draft.mainPosts = draft.mainPosts.filter((v) => v.id !== action.data);
+        break;
+      }
+      case REMOVE_POST_FAILURE:
+        draft.removePostLoading = false;
+        draft.removePostDone = action.error;
+        break;
+      case ADD_COMMENT_REQUEST:
+        draft.addCommentLoading = true;
+        draft.addCommentDone = false;
+        draft.addCommentError = null;
+        break;
+      case ADD_COMMENT_SUCCESS: 
+        const post = draft.mainPosts.find((v) => v.id === action.data.postId);
+        post.Comments.unshift(dummyComment(action.data));
+        draft.addCommentLoading = false;
+        draft.addCommentDone = true;
+        break;
+      case ADD_COMMENT_FAILURE:
+        draft.addCommentLoading = false;
+        draft.addCommentDone = action.error;
+        break;
+      default:
+        break;
     }
-    case ADD_COMMENT_FAILURE:
-      return {
-        ...state,
-        addCommentLoading: false,
-        addCommentDone: action.error,
-      };
-    
-    case REMOVE_POST_REQUEST:
-      return {
-        ...state,
-        removePostLoading: true,
-        removePostDone: false,
-        removePostError: null,
-      };
-    case REMOVE_POST_SUCCESS: {
-      return {
-        ...state,
-        mainPosts : state.mainPosts.filter((v) => v.id !== action.data),
-        removePostLoading: false,
-        removePostDone: true,
-      };
-    }
-    case REMOVE_POST_FAILURE:
-      return {
-        ...state,
-        removePostLoading: false,
-        removePostDone: action.error,
-      };
-    
-    default:
-      return state;
-  }
+  });
 };
 
 export default reducer;
--- sagas/post.js
+++ sagas/post.js
@@ -4,9 +4,9 @@ import shortId from 'shortid';
 import { 
   ADD_POST_SUCCESS, ADD_POST_REQUEST, ADD_POST_FAILURE,
    ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, REMOVE_POST_REQUEST, 
-   REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE
+   REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE,
 } from '../reducers/post';
-import { ADD_POST_TO_ME } from '../reducers/user';
+import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from '../reducers/user';
 
 function addCommentAPI(data) {
   return axios.post(`/api/post/${data.postId}/comment`, data);
@@ -19,10 +19,7 @@ function* addComment(action) {
     const id = shortId.generate();
     yield put({
       type: ADD_COMMENT_SUCCESS,
-      data : {
-        id,
-        content : action.data
-      }
+      data : action.data
     });
   } catch (err) {
     console.log(err);
@@ -78,6 +75,7 @@ function* removePost(action) {
       data : action.data
     })
   } catch (err) {
+    console.log(err);
     yield put({
       type: REMOVE_POST_FAILURE,
       data: err.response.data,
